658621efb2f5474aa73f10c6939baee9fb1973aa
ghanshyam-du <ghanshyamd816@gmail.com>
2025-07-23 23:25:26 +0530

    update

--- Sorting Tech/SelectionSort.java
+++ Sorting Tech/SelectionSort.java
@@ -5,15 +5,15 @@ public class SelectionSort {
         int[] arr = {7,5,9,2,8};
        
         for(int i = 0; i< arr.length ; i++){
-            int mini = i;
+            int min = i;
 
             for(int j = i + 1; j < arr.length ; j++){
-                if(arr[j] < arr[mini]){
-                    mini = j;
+                if(arr[j] < arr[min]){
+                    min = j;
                 }
             }
-            int temp = arr[mini];
-            arr[mini] = arr[i];
+            int temp = arr[min];
+            arr[min] = arr[i];
             arr[i] = temp;
         }
 
