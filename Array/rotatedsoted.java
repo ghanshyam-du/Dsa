bf422f2fc963d4667b3401baab264d245954333a
ghanshyam-du <ghanshyamd816@gmail.com>
2025-08-13 23:35:45 +0530

    how many times is a sorted array is rotated

---
+++ Array/rotatedsoted.java
@@ -0,0 +1,27 @@
+public class rotatedsoted {
+    public static void main(String args[]){
+        class Solution {
+    public boolean check(int[] nums) {
+        
+        int cnt = 0; 
+        int n = nums.length;
+
+        if(nums[0] < nums[n - 1]){
+            cnt++;
+        }
+
+        for(int i = 1; i< n; i++){
+            if(nums[i-1] > nums[i]){
+                cnt++;
+
+                if(cnt > 1){
+                    return false;
+                }
+            }
+        }
+        return true;
+
+    }
+}
+    }
+}
