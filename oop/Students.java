caee7eabb342c9af5aad5d1273ada380a828a602
ghanshyam-du <ghanshyamd816@gmail.com>
2025-08-16 18:57:48 +0530

    oops

--- oop/Students.java
+++ oop/Students.java
@@ -1,13 +1,14 @@
 public class Students {
     public static void main(String args[]){
-        Student Ghanshyam = new Student();
-        Ghanshyam.roll = 21;
-        Ghanshyam.marks = 90.23f;
-        Ghanshyam.name = "Ghanshyam dubey ";
+        Student Ghanshyam = new Student(25, "Ghanshyam", 98);
+        // Ghanshyam.roll = 21;
+        // Ghanshyam.marks = 90.23f;
+        // Ghanshyam.name = "Ghanshyam dubey ";
 
         System.out.println(Ghanshyam.marks);
         System.out.println(Ghanshyam.name);
         System.out.println(Ghanshyam.roll);
+        Ghanshyam.greeting();
 
     }
 }
@@ -16,4 +17,13 @@ class Student{
     int roll;
     String name;
     float marks;
+
+    Student(int rol, String naam, float marks){
+        this.roll = rol;
+        this.name = naam;
+        this.marks = marks;
+    }
+  void  greeting(){
+        System.out.println("Hi my name is "+ this.name);
+    }
 }
