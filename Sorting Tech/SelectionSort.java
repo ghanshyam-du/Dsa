d8ebd5bb17d0e1c1ca0cea60cf7362000e8cb120
ghanshyam-du <ghanshyamd816@gmail.com>
2025-08-20 22:11:23 +0530

    SelectionSort

--- Sorting Tech/SelectionSort.java
+++ Sorting Tech/SelectionSort.java
@@ -2,26 +2,30 @@
 
 public class SelectionSort {
     public static void main(String args[]){
-        int[] arr = {7,5,9,2,8};
-       
-        for(int i = 0; i< arr.length ; i++){
-            int mini = i;
+        int[] arr = {29,10,14,37,13};
 
-            for(int j = i + 1; j < arr.length ; j++){
-                if(arr[j] < arr[mini]){
-                    mini = j;
+        selection(arr);
+
+        for(int num : arr){
+            System.out.println(num+" ");
+        }
+
+       
+    }
+    public static void selection(int[] arr){
+        int n = arr.length;
+        for(int i = 0; i< n; i++){
+            int min = i;
+            for(int j = i + 1; j< n; j++){
+                if(arr[j] < arr[min]){
+                    min = j;
                 }
             }
-            int temp = arr[mini];
-            arr[mini] = arr[i];
-            arr[i] = temp;
-        }
 
-        for(int i = 0; i< arr.length; i++){
-            System.out.print( arr[i] );
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] =temp;
         }
-
-       
     }
     
 }
