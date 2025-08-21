a4d9b40bf50216a3b045f81d992da5ea42d67746
ghanshyam-du <ghanshyamd816@gmail.com>
2025-08-21 13:44:51 +0530

    Insertion sort

---
+++ Sorting Tech/InsertionSort.java
@@ -0,0 +1,26 @@
+public class InsertionSort {
+    public static void main(String args[]){
+        int[] arr = {6,1,7,4,2,9,8,5,3};
+
+        insertion(arr);
+
+        for(int num : arr){
+            System.out.print(num + " ");
+        }
+    }
+
+    private static void insertion(int[] arr) {
+        
+        for(int i = 1; i< arr.length; i++){
+            int temp = arr[i];
+            int j = i - 1;
+
+            while(j >= 0 && arr[j] > temp){
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j  + 1] = temp;
+        
+        }
+    }
+}
