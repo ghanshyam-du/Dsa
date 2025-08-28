d6cc69b28191c342d60ffdee4fd4ecf515be59cf
ghanshyam-du <ghanshyamd816@gmail.com>
2025-08-28 21:09:28 +0530

    updated

--- impact/Reverse.java
+++ impact/Reverse.java
@@ -1,19 +1,19 @@
 import java.util.Arrays;
 
 public class Reverse {
-    public static void main(String[] args){
-        int[] arr = {1,2,3,4,5,6,7,8,9,10,11};
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
         int n = arr.length;
         int start = 0;
-        int end = n-1;
+        int end = n - 1;
 
-        while(start < end){
+        while (start < end) {
             int temp = arr[start];
             arr[start] = arr[end];
             arr[end] = temp;
             start++;
 
-            end--;      
+            end--;
         }
         System.out.print(Arrays.toString(arr));
     }
