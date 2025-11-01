1b35f7831d824878d584ddb492ccf78a543b3d3d
ghanshyam-du <ghanshyamd816@gmail.com>
2025-11-01 14:33:54 +0530

    updated

--- stack and queues/index.js
+++ stack and queues/index.js
@@ -1,9 +1,23 @@
-let stack = []
-stack.push(1);
-stack.push(2);
-stack.push(3);
-console.log(stack);
-stack.pop();
+// let stack = []
+// stack.push(1);
+// stack.push(2);
+// stack.push(3);
+// console.log(stack);
 // stack.pop();
-console.log(stack);
-console.log(stack[stack.length-1]);
+// // stack.pop();
+// console.log(stack);
+// console.log(stack[stack.length-1]);
+
+
+let q = [];
+
+q.push(1)
+q.push(2)
+q.push(3)
+q.push(4)
+q.push(5)
+console.log(q)
+q.shift();
+console.log(q);
+let front = q[0];
+console.log(front);
