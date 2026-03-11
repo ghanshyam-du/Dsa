e7f457a76a21136b3b57b96b391abd206192905c
Ghanshyam <135561990+ghanshyam-du@users.noreply.github.com>
2026-03-11 23:10:12 +0530

    Time: 0 ms (100%), Space: 48.1 MB (68.68%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,61 +1,52 @@
 class Solution {
-    public static int firstPos(int[] nums, int target){
+    public static int fistP(int[] nums, int target){
         int start = 0;
-        int last = nums.length - 1; 
+        int end = nums.length-1;
         int res = -1;
+        while(start <= end){
+            int mid = (start + end)/2;
 
-
-        while(start <=  last){
-            int mid =(start + last)/2;
-
-            if(nums[mid] == target){
+            if(target == nums[mid]){
                 res = mid;
-                last = mid - 1;
+                end = mid - 1;
             }
-            else if(nums[mid] > target){
-
-                last = mid - 1;
-            }
-            else{
+            else if(target < nums[mid]){
+                end = mid - 1;
+            }else{
                 start = mid + 1;
             }
         }
-
         return res;
     }
 
-
-
-     public static int lastPos(int[] nums, int target){
+   public static int lastP(int[] nums, int target){
         int start = 0;
-        int last = nums.length - 1; 
+        int end = nums.length-1;
         int res = -1;
+        while(start <= end){
+            int mid = (start + end)/2;
 
-
-        while(start <=  last){
-            int mid =(start + last)/2;
-
-            if(nums[mid] == target){
+            if(target == nums[mid]){
                 res = mid;
                 start = mid + 1;
             }
-            else if(nums[mid] > target){
-
-                last = mid - 1;
-            }
-            else{
+            else if(target < nums[mid]){
+                end = mid - 1;
+            }else{
                 start = mid + 1;
             }
         }
-
         return res;
     }
-    public int[] searchRange(int[] nums, int target) {
-        int n1 =  firstPos(nums, target);
-        int n2 =  lastPos(nums, target);
-        int[] arr = new int[] {n1, n2};
+    
+ public int[] searchRange(int[] nums, int target) {
 
-        return arr;
-        
+    int first = fistP(nums, target); 
+    int last = lastP(nums, target); 
+    int[] arr = new int[]{first, last};
+    
+    return arr;
     }
 }
+
+
