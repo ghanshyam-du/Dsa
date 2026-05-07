6e1faae7539808b2d1e0046619f66f3982834790
Ghanshyam <135561990+ghanshyam-du@users.noreply.github.com>
2026-05-07 18:23:52 +0530

    Time: 32 ms (33.49%), Space: 54.6 MB (66.8%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,18 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let map = {};
-    for(let i = 0; i <= nums.length; i++){
-        map[nums[i]] = i;
 
-    }
-
-    for(let i = 0; i<= nums.length; i++){
-        let value = target - nums[i];
-
-        if(map[value] && map[value] != i )
-        {
-            return [i, map[value]];
+    for(let i = 0; i< nums.length; i++){
+        for(let j = i + 1; j < nums.length; j++){
+            if(nums[i] + nums[j] == target){
+                return [i, j];
+            }
         }
     }
+    
 };
