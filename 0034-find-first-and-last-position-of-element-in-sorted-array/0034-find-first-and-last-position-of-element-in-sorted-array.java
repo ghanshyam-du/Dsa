dfb2cf727fcb538c10e64046588e8ead94106a15
Ghanshyam <135561990+ghanshyam-du@users.noreply.github.com>
2026-05-08 09:04:33 +0530

    Time: 0 ms (100%), Space: 48.1 MB (50.34%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,52 +1,49 @@
 class Solution {
-    public static int fistP(int[] nums, int target){
+    public static int first(int[] nums, int target){
         int start = 0;
-        int end = nums.length-1;
+        int end = nums.length - 1;
         int res = -1;
-        while(start <= end){
-            int mid = (start + end)/2;
 
-            if(target == nums[mid]){
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            if(nums[mid] == target){
                 res = mid;
                 end = mid - 1;
-            }
-            else if(target < nums[mid]){
+            }else if(nums[mid] >= target){
                 end = mid - 1;
             }else{
                 start = mid + 1;
             }
         }
         return res;
+
     }
-
-   public static int lastP(int[] nums, int target){
+    public static int last(int[] nums, int target){
         int start = 0;
-        int end = nums.length-1;
+        int end = nums.length - 1;
         int res = -1;
-        while(start <= end){
-            int mid = (start + end)/2;
 
-            if(target == nums[mid]){
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            if(nums[mid] == target){
                 res = mid;
                 start = mid + 1;
-            }
-            else if(target < nums[mid]){
+            }else if(nums[mid] >= target){
                 end = mid - 1;
             }else{
                 start = mid + 1;
             }
         }
         return res;
-    }
-    
- public int[] searchRange(int[] nums, int target) {
 
-    int first = fistP(nums, target); 
-    int last = lastP(nums, target); 
-    int[] arr = new int[]{first, last};
-    
-    return arr;
+    }
+    public int[] searchRange(int[] nums, int target) {
+        int firstN = first(nums, target);
+        int lastN = last(nums, target);
+
+        return new int[]{firstN, lastN};
+        
     }
 }
-
-
