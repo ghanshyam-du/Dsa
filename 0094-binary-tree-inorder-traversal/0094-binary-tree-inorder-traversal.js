59c01723f1964949d522e898fcb4b503f10e31f4
Ghanshyam <135561990+ghanshyam-du@users.noreply.github.com>
2026-05-12 11:34:39 +0530

    Time: 0 ms (100%), Space: 53.7 MB (64.9%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
@@ -12,17 +12,29 @@
  */
 var inorderTraversal = function(root) {
     let ans = [];
+    let stack = [];
+    let crnt = root;
 
-    function traversal(crnt){
-        //left -> root -> right
-        if(!crnt) return;
-
-        traversal(crnt.left);
+    while(crnt || stack.length){
+        while(crnt){
+         stack.push(crnt);
+           crnt = crnt.left;
+        }
+        crnt = stack.pop();
         ans.push(crnt.val);
-        traversal(crnt.right);
-
+        crnt = crnt.right;
     }
-    traversal(root);
+
+    // function traversal(crnt){
+    //     //left -> root -> right
+    //     if(!crnt) return;
+
+    //     traversal(crnt.left);
+    //     ans.push(crnt.val);
+    //     traversal(crnt.right);
+
+    // }
+    // traversal(root);
 
     return ans;
     
