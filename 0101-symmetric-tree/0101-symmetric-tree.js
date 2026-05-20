bc0d73a490c10cbd5ac0cd3a924b4b1070d431ae
Ghanshyam <135561990+ghanshyam-du@users.noreply.github.com>
2026-05-20 12:49:36 +0530

    Time: 2 ms (5.37%), Space: 58 MB (17.75%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.js
+++ 0101-symmetric-tree/0101-symmetric-tree.js
@@ -12,14 +12,37 @@
  */
 var isSymmetric = function (root) {
 
+    let q = [root.left, root.right];
 
-    let isMirror = (left, right) => {
-        if (!left && !right) return true;
-        if (!left || !right) return false;
-        return left.val === right.val &&
-            isMirror(left.left, right.right) &&
-            isMirror(left.right, right.left);
+    while (q.length) {
+        let q1 = q.shift();
+        let q2 = q.shift();
+
+        if (!q1 && !q2) {
+            continue;
+        }
+        if (!q1 || !q2) {
+            return false;
+        }
+        if (q1.val != q2.val) {
+            return false;
+        }
+
+
+        q.push(q1.left, q2.right);
+        q.push(q1.right, q2.left);
     }
-    return isMirror(root.left, root.right);
+
+    return true;
+
+
+    // let isMirror = (left, right) => {
+    //     if (!left && !right) return true;
+    //     if (!left || !right) return false;
+    //     return left.val === right.val &&
+    //         isMirror(left.left, right.right) &&
+    //         isMirror(left.right, right.left);
+    // }
+    // return isMirror(root.left, root.right);
 
 };
