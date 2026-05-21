061c49951146a2d98e78c01d510fa0c0bf6d1b07
Ghanshyam <135561990+ghanshyam-du@users.noreply.github.com>
2026-05-21 17:31:00 +0530

    Time: 0 ms (100%), Space: 55 MB (86.6%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.js
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.js
@@ -11,30 +11,47 @@
  * @return {number[][]}
  */
 var zigzagLevelOrder = function(root) {
-     let ans = [];
-    if(!root) return ans;
-    let q  = [root];
-    let level = 0;
+    let ans = [];
+    if(!root) return [];
 
-    while(q.length){
-        let levelArr = [];
-        let levelSize = q.length;
+    var zigZag = (curr, level) =>{
+        if(!ans[level]) ans[level] = [];
 
-        for(let i = 0; i< levelSize ; i++){
-            let curr = q.shift();
-            if(level % 2 == 0){
-                levelArr.push(curr.val);
-            }
-            else{
-                levelArr.unshift(curr.val);
-            }
-            curr.left && q.push(curr.left);
-           curr.right &&  q.push(curr.right);
+        if(level % 2 == 0){
+            ans[level].push(curr.val);
+        }else{
+            ans[level].unshift(curr.val);
         }
-        ans.push(levelArr);
-        ++level;
+        curr.left &&  zigZag(curr.left, level+1);
+        curr.right && zigZag(curr.right, level+1);
 
     }
+    zigZag(root, 0);
+    
+    //  let ans = [];
+    // if(!root) return ans;
+    // let q  = [root];
+    // let level = 0;
+
+    // while(q.length){
+    //     let levelArr = [];
+    //     let levelSize = q.length;
+
+    //     for(let i = 0; i< levelSize ; i++){
+    //         let curr = q.shift();
+    //         if(level % 2 == 0){
+    //             levelArr.push(curr.val);
+    //         }
+    //         else{
+    //             levelArr.unshift(curr.val);
+    //         }
+    //         curr.left && q.push(curr.left);
+    //        curr.right &&  q.push(curr.right);
+    //     }
+    //     ans.push(levelArr);
+    //     ++level;
+
+    // }
 
     return ans;
     
