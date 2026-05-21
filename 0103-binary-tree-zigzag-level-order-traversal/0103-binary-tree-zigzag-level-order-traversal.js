d54d85a5d9302b9030624988659adf7acac73942
Ghanshyam <135561990+ghanshyam-du@users.noreply.github.com>
2026-05-21 17:20:10 +0530

    Time: 0 ms (100%), Space: 55.9 MB (72.84%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.js
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.js
@@ -11,30 +11,29 @@
  * @return {number[][]}
  */
 var zigzagLevelOrder = function(root) {
-    if(!root) return [];
-    let ans = [];
-    let q = [root];
-    level = 0;
+     let ans = [];
+    if(!root) return ans;
+    let q  = [root];
+    let level = 0;
 
     while(q.length){
         let levelArr = [];
         let levelSize = q.length;
 
-        for(let i = 0; i< levelSize; i++){
+        for(let i = 0; i< levelSize ; i++){
             let curr = q.shift();
-
             if(level % 2 == 0){
                 levelArr.push(curr.val);
             }
             else{
                 levelArr.unshift(curr.val);
             }
-
             curr.left && q.push(curr.left);
-            curr.right && q.push(curr.right);
+           curr.right &&  q.push(curr.right);
         }
         ans.push(levelArr);
         ++level;
+
     }
 
     return ans;
