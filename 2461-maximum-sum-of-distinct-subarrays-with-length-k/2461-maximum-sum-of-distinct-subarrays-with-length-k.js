f184065bd83a3aacc2445488d58fae1a499220e1
Ghanshyam <135561990+ghanshyam-du@users.noreply.github.com>
2026-05-28 04:49:23 +0530

    Time: 77 ms (29.37%), Space: 90.1 MB (15.05%) - LeetHub

---
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maximumSubarraySum = function(nums, k) {
+    let max = 0;
+    let sum = 0;
+    let map = new Map();
+    for(let i = 0; i< k; i++){
+        sum += nums[i];
+        map.set(nums[i], (map.get(nums[i]) || 0) + 1);
+    }
+    if(map.size === k){
+        max = sum;
+    }
+
+    for(let i = k; i< nums.length; i++){
+       
+        sum = sum - nums[i - k] + nums[i];
+
+        map.set(nums[i - k], (map.get(nums[i - k]) - 1));
+
+        if(map.get(nums[i - k])=== 0){
+            map.delete(nums[i - k]);
+        }
+
+
+
+        map.set(nums[i], (map.get(nums[i]) || 0) + 1);
+
+        if(map.size === k){
+            max = Math.max(max, sum);
+        }
+    }
+    return max;
+    
+};
