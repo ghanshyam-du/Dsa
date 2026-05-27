deb2dd2eb38c4e2d66f446f36c98b61710d63d15
Ghanshyam <135561990+ghanshyam-du@users.noreply.github.com>
2026-05-27 18:16:02 +0530

    Time: 2 ms (82.4%), Space: 68.9 MB (52.03%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
@@ -4,17 +4,20 @@
  * @return {number}
  */
 var findMaxAverage = function (nums, k) {
-   let max = 0;
+    if(nums.length == 1) return nums[0]/k;
+    let sum = 0;
+    let max = 0;
 
-   for(let i = 0; i< k; i++){
-      max += nums[i];
-   }
+    for(let i = 0; i< k  ;i++){
+        sum += nums[i];
+    }
+    max = sum;
+    for(let i = k; i< nums.length; i++){
+        sum = sum - nums[i - k] + nums[i];
+        max = Math.max(max, sum);
+        
+    }
 
-   let ans = max;
-   for(let i = k; i< nums.length; i++){
-       max = max - nums[i - k] + nums[i];
-       ans = Math.max(ans, max);
-   }
-
-   return ans/k;
+    return max/k;
+   
 };
