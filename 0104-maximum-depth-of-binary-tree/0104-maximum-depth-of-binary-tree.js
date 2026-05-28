3bfe452ef7729fdddd5988b5dfc1fd70c1163215
Ghanshyam <135561990+ghanshyam-du@users.noreply.github.com>
2026-05-29 05:25:03 +0530

    Time: 2 ms (18.85%), Space: 57.1 MB (68.99%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -10,27 +10,19 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var maxDepth = function(root) {
-    if(!root) return 0;    // this is bottom up approach 
+var maxDepth = function (root) {
+    let count = 0;
 
-    let leftM = maxDepth(root.left);
-    let rightM = maxDepth(root.right); 
-     
-    return 1 + Math.max(leftM, rightM);
+    function traverse(crnt, cnt) {
+        if (!crnt) return 0;
+
+        count = Math.max(count, cnt);
+        crnt.left && traverse(crnt.left, cnt + 1);
+        crnt.right && traverse(crnt.right, cnt + 1);
+
+    }
+    traverse(root, 1);
+    return count;
 
 
-    // if(!root) return 0;  this is the top down approch 
-    // let Mdepth = 0;
-
-    // var traverse = (curr, depth)=>{
-    //     Mdepth = Math.max(Mdepth, depth);
-
-    //     curr.left && traverse(curr.left, depth+1);
-    //     curr.right && traverse(curr.right, depth+1);
-
-    // }
-    // traverse(root, 1);
-
-    // return Mdepth;
-    
 };
