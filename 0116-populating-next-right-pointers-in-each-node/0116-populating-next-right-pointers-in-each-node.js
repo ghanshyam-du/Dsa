e3e52be0790bf4b4b510e122fbefabe911ef3475
Ghanshyam <135561990+ghanshyam-du@users.noreply.github.com>
2026-06-01 04:48:18 +0530

    Time: 56 ms (59.14%), Space: 58.3 MB (96.92%) - LeetHub

---
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.js
@@ -0,0 +1,37 @@
+/**
+ * // Definition for a _Node.
+ * function _Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {_Node} root
+ * @return {_Node}
+ */
+var connect = function(root) {
+   if(!root) return root;
+   
+   let traverse= (crnt) =>{
+
+    if(crnt.left){
+        crnt.left.next = crnt.right;
+    }
+    if(crnt.next && crnt.right){
+        crnt.right.next = crnt.next.left;
+    }
+
+
+   crnt.left && traverse(crnt.left);
+   crnt.right && traverse(crnt.right);
+
+   }
+   traverse(root);
+
+   return root;
+   
+    
+};
