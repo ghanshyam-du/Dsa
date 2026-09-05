560862ab39be636c8429ecf62180358ae9ca706e
Ghanshyam <135561990+ghanshyam-du@users.noreply.github.com>
2026-09-05 11:57:58 +0530

    Time: 252 ms (5.35%), Space: 77.9 MB (7.3%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -4,10 +4,91 @@
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
-   nums.sort((a,b) => a - b);
-   let a = nums.length - k;
-   console.log(nums)
-   console.log("vvvv",a);
 
-   return nums[a];
+    class MinHeap {
+        constructor() {
+            this.heap = [];
+        }
+
+        insert(value) {
+            this.heap.push(value);
+
+            let index = this.heap.length - 1;
+
+            while (index > 0) {
+                let parent = Math.floor((index - 1) / 2);
+
+                if (this.heap[index] >= this.heap[parent]) {
+                    break;
+                }
+
+                [this.heap[index], this.heap[parent]] =
+                [this.heap[parent], this.heap[index]];
+
+                index = parent;
+            }
+        }
+
+        remove() {
+            let root = this.heap[0];
+            let last = this.heap.pop();
+
+            if (this.heap.length > 0) {
+                this.heap[0] = last;
+
+                let index = 0;
+
+                while (true) {
+                    let left = 2 * index + 1;
+                    let right = 2 * index + 2;
+                    let smallest = index;
+
+                    if (
+                        left < this.heap.length &&
+                        this.heap[left] < this.heap[smallest]
+                    ) {
+                        smallest = left;
+                    }
+
+                    if (
+                        right < this.heap.length &&
+                        this.heap[right] < this.heap[smallest]
+                    ) {
+                        smallest = right;
+                    }
+
+                    if (smallest === index) {
+                        break;
+                    }
+
+                    [this.heap[index], this.heap[smallest]] =
+                    [this.heap[smallest], this.heap[index]];
+
+                    index = smallest;
+                }
+            }
+
+            return root;
+        }
+
+        peek() {
+            return this.heap[0];
+        }
+
+        size() {
+            return this.heap.length;
+        }
+    }
+
+    let minHeap = new MinHeap();
+
+    for (let num of nums) {
+        minHeap.insert(num);
+
+        if (minHeap.size() > k) {
+            minHeap.remove();
+        }
+    }
+
+    return minHeap.peek();
 };
