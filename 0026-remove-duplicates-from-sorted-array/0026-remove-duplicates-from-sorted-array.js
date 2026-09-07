89b543f27e0a0298746554afd278b9eb2550d778
Ghanshyam <135561990+ghanshyam-du@users.noreply.github.com>
2026-09-07 09:05:56 +0530

    Time: 0 ms (100%), Space: 57.8 MB (56.55%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let j = 0;
+    
+    for( let i = 0; i <= nums.length; i++){
+           
+        if(nums[j] < nums[i]){
+            j++;
+            nums[j] = nums[i];
+        }
+    }
+
+    return j + 1;
+};
