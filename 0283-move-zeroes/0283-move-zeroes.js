58565e81b11693c6cc1cd92c870207d77e3b0cf7
Ghanshyam <135561990+ghanshyam-du@users.noreply.github.com>
2026-09-08 10:25:05 +0530

    Time: 3 ms (38.05%), Space: 60.2 MB (62.14%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -6,13 +6,13 @@ var moveZeroes = function(nums) {
     let j = 0;
     for(let i = 0; i < nums.length; i++){
         if(nums[i] !== 0){
-            nums[j] = nums[i];
+           [nums[i], nums[j]]= [nums[j], nums[i]];
             j++;
         }
     }
 
-    while(j< nums.length){
-        nums[j] = 0;
-        j++
-    }
+    // while(j< nums.length){
+    //     nums[j] = 0;
+    //     j++
+    // }
 };
